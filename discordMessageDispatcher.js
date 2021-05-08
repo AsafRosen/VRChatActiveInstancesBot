568c68004052814d0c9a316da9ac88abd112be4c
Asaf Rosen <frizben@gmail.com>
2021-05-08 17:25:54 +0300

    Split up message formatting and cleaned up a bit

---
+++ discordMessageDispatcher.js
@@ -0,0 +1,76 @@
+import * as discordBot from "./discordBot.js";
+import * as vrcBot from "./vrcBot.js";
+
+const STATUS_TO_EMOJI = {
+    'join me': '🔵',
+    'active': '🟢',
+    'ask me': '🟠',
+    'busy': '🔴',
+  };
+
+function renderUserList(users) {
+    if (!users || users.length === 0) {
+      return "No one";
+    }
+  
+    const userList = users
+      .map(
+        (user) => `${STATUS_TO_EMOJI[user.status] ?? '❔'} [${user.displayName}](https://vrchat.com/home/user/${user.id})`
+      )
+      .join("\n");
+  
+    if (userList.length < 1024) {
+      return userList;
+    }
+  
+    return users.map((user) => `${STATUS_TO_EMOJI[user.status] ?? '❔'} ${user.displayName}`).join("\n");
+  }
+  
+  export async function postInstanceDetails(instance, users) {
+    let world, instanceDetails;
+  
+    world = await vrcBot.getWorldDetails(instance.split(":")[0]);
+    instanceDetails = await vrcBot.getInstanceDetails(instance);
+  
+    await discordBot.postMessage(
+      {
+        embed: {
+          title: `${world.name} ${instanceDetails.name}`,
+          url: `https://vrchat.com/home/launch?worldId=${
+            world.id
+          }&instanceId=${encodeURIComponent(instanceDetails.instanceId)}`,
+          description: `Online: ${users.length} friends, ${instanceDetails.n_users} players`,
+          timestamp: new Date().toISOString(),
+          fields: [
+            {
+              name: "Who is in here?",
+              value: renderUserList(users),
+            },
+          ],
+          image: {
+            url: world.imageUrl,
+          },
+        },
+      },
+      instance
+    );
+  }
+  
+  export async function postOnlineFriends(onlineFriends) {
+    await discordBot.postMessage(
+      {
+        embed: {
+          title: "Online People",
+          description: `${onlineFriends.length} friends`,
+          timestamp: new Date().toISOString(),
+          fields: [
+            {
+              name: "Who's online?",
+              value: renderUserList(onlineFriends),
+            },
+          ],
+        },
+      },
+      "online"
+    );
+  }
--- statusUpdaterJob.js
+++ statusUpdaterJob.js
@@ -1,85 +1,7 @@
 import * as vrcBot from "./vrcBot.js";
 import * as discordBot from "./discordBot.js";
+import * as discordMessageDispatcher from "./discordMessageDispatcher.js";
 import groupBy from "group-by";
-
-
-const STATUS_TO_EMOJI = {
-  'join me': '🔵',
-  'active': '🟢',
-  'ask me': '🟠',
-  'busy': '🔴',
-};
-
-function arrayEquals(a, b) {
-  return a.length === b.length && a.every((v, i) => v === b[i]);
-}
-
-function renderUserList(users) {
-  if (!users || users.length === 0) {
-    return "No one";
-  }
-
-  const userList = users
-    .map(
-      (user) => `${STATUS_TO_EMOJI[user.status] ?? '❔'} [${user.displayName}](https://vrchat.com/home/user/${user.id})`
-    )
-    .join("\n");
-
-  if (userList.length < 1024) {
-    return userList;
-  }
-
-  return users.map((user) => `${STATUS_TO_EMOJI[user.status] ?? '❔'} ${user.displayName}`).join("\n");
-}
-
-async function postInstanceDetails(instance, users) {
-  let world, instanceDetails;
-
-  world = await vrcBot.getWorldDetails(instance.split(":")[0]);
-  instanceDetails = await vrcBot.getInstanceDetails(instance);
-
-  await discordBot.postMessage(
-    {
-      embed: {
-        title: `${world.name} ${instanceDetails.name}`,
-        url: `https://vrchat.com/home/launch?worldId=${
-          world.id
-        }&instanceId=${encodeURIComponent(instanceDetails.instanceId)}`,
-        description: `Online: ${users.length} friends, ${instanceDetails.n_users} players`,
-        timestamp: new Date().toISOString(),
-        fields: [
-          {
-            name: "Who is in here?",
-            value: renderUserList(users),
-          },
-        ],
-        image: {
-          url: world.imageUrl,
-        },
-      },
-    },
-    instance
-  );
-}
-
-async function postOnlineFriends(onlineFriends) {
-  await discordBot.postMessage(
-    {
-      embed: {
-        title: "Online People",
-        description: `${onlineFriends.length} friends`,
-        timestamp: new Date().toISOString(),
-        fields: [
-          {
-            name: "Who's online?",
-            value: renderUserList(onlineFriends),
-          },
-        ],
-      },
-    },
-    "online"
-  );
-}
 
 export async function run() {
   try {
@@ -88,7 +10,7 @@ export async function run() {
       .filter((status) => status != null)
       .filter((status) => status.location !== "offline");
 
-    postOnlineFriends(onlineFriends);
+    discordMessageDispatcher.postOnlineFriends(onlineFriends);
 
     const joinableFriends = onlineFriends.filter(
       (status) => status.location !== "private"
@@ -109,27 +31,14 @@ export async function run() {
       {}
     );
 
-    if (
-      !arrayEquals(
-        ["online"].concat(Object.keys(friendsByInstance)),
-        Object.keys(discordBot.getFixedMessages())
-      )
-    ) {
-      console.info(
-        `[${new Date().toISOString()}] Active Instances Changed: [${Object.keys(
-          friendsByInstance
-        ).join(", ")}]`
-      );
-    }
-
     for (const [instance, users] of Object.entries(friendsByInstance)) {
-      postInstanceDetails(instance, users);
+      discordMessageDispatcher.postInstanceDetails(instance, users);
     }
 
     await discordBot.clearMessagesNotInIDs(
       Object.keys(friendsByInstance).concat(["online"])
     );
   } catch (error) {
-    console.error("Update job failed", error);
+    console.error("Update job failed", error.message);
   }
 }
